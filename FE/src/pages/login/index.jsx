e5cf3a199e486658b99d644f0dd4a7a2214801cd
QN53 <quangnhut1475369@gmail.com>
2024-09-28 11:14:18 +0700

    login

--- FE/src/pages/login/index.jsx
+++ FE/src/pages/login/index.jsx
@@ -1,37 +1,37 @@
 // import React from 'react'
 import AuthenTemplate from "../../components/authen-template";
 import { Form, Input } from "antd";
-import { gooleProvider } from "../../config/firebase";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { gooleProvider } from "../../config/firebase";
+// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 function LoginPage() {
-  const handleLoginGoole = () => {
-    const auth = getAuth();
-    signInWithPopup(auth, gooleProvider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
+  // const handleLoginGoole = () => {
+  //   const auth = getAuth();
+  //   signInWithPopup(auth, gooleProvider)
+  //     .then((result) => {
+  //       // This gives you a Google Access Token. You can use it to access the Google API.
+  //       const credential = GoogleAuthProvider.credentialFromResult(result);
+  //       const token = credential.accessToken;
+  //       // The signed-in user info.
+  //       const user = result.user;
 
-        console.log(user);
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-      })
-      .catch((error) => {
-        console.log(error);
-        // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
+  //       console.log(user);
+  //       // IdP data available using getAdditionalUserInfo(result)
+  //       // ...
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       // Handle Errors here.
+  //       // const errorCode = error.code;
+  //       // const errorMessage = error.message;
 
-        // The email of the user's account used.
-        // const email = error.customData.email;
-        // The AuthCredential type that was used.
-        // const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
-  };
+  //       // The email of the user's account used.
+  //       // const email = error.customData.email;
+  //       // The AuthCredential type that was used.
+  //       // const credential = GoogleAuthProvider.credentialFromError(error);
+  //       // ...
+  //     });
+  //  };
   return (
     <AuthenTemplate>
       <Form labelCol={{ span: 24 }}>
@@ -43,7 +43,7 @@ function LoginPage() {
         </Form.Item>
 
         <button>Login</button>
-        <button onClick={handleLoginGoole}>Login Goole</button>
+        {/* <button onClick={handleLoginGoole}>Login Goole</button> */}
       </Form>
     </AuthenTemplate>
   );
