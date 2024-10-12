c6ee0e86a46c47aa3594c08f9eef51fd5d5fb86a
Khuất Trường Huy <cloneacc241@gmail.com>
2024-10-12 10:12:38 +0700

    quick patch axios.js

--- FE/src/config/axios.js
+++ FE/src/config/axios.js
@@ -1,9 +1,21 @@
 import axios from "axios";
-import { https } from "axios";
 
-const api = axios.create({
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false,
-  }),
-});
+const baseUrl = "https://localhost:7229";
+
+const config = {
+  baseUrl: baseUrl,
+};
+
+const api = axios.create(config);
+
+api.defaults.baseURL = baseUrl;
+
+const handleBefore = (config) => {
+  const token = localStorage.getItem("token")?.replaceAll('"', "");
+  config.header["Authorization"] = `Bearer ${token}`;
+  return config;
+};
+
+api.interceptors.request.use(handleBefore, null);
+
 export default api;
